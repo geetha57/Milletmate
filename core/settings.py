b8649194b5903deb183aef07fcb6ddf749de0742
Geetha sri <geethavelagaleti5752gmail.com>
2026-03-21 15:08:07 +0530

    final csrf fix

--- core/settings.py
+++ core/settings.py
@@ -13,11 +13,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-this-later')
 DEBUG = True  # Set to False in production
 ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
+
 CSRF_TRUSTED_ORIGINS = [
-    "https://welcoming-dedication-production.up.railway.app"
+    "https://welcoming-dedication-production.up.railway.app",
+    "http://welcoming-dedication-production.up.railway.app"
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # ---------------- AUTH ----------------
 # Custom User Model to handle Farmer, Buyer, and Admin roles
