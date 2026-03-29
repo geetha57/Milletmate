51d4b95858c4f13054d5027081003045230b828d
Geetha sri <geethavelagaleti5752gmail.com>
2026-03-29 09:21:58 +0530

    final media fix

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,4 @@ urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
